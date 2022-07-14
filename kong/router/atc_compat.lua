d4433ccceca2fc9ae1477d392484a766bf4556d5
chronolaw <chrono_cpp@me.com>
2022-07-14 21:27:16 +0800

    style fix

--- kong/router/atc_compat.lua
+++ kong/router/atc_compat.lua
@@ -11,7 +11,7 @@ local server_name = require("ngx.ssl").server_name
 local normalize = require("kong.tools.uri").normalize
 local hostname_type = require("kong.tools.utils").hostname_type
 local tb_nkeys = require("table.nkeys")
-local tablepool = require "tablepool"
+local tablepool = require("tablepool")
 
 
 local ngx = ngx
